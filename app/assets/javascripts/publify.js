3c4997000342f11ca2050785f315bab0ba013be9
Juuro <mail@sebastian-engel.de>
2014-09-01 23:52:11 +0200

    Positioning of date nearby the article.

--- app/assets/javascripts/publify.js
+++ app/assets/javascripts/publify.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 //= require_self
 // Front javascript manifest
+
+$(document).ready(function() {
+  $(".h-entry").each(function( index ) {
+    var height = $( this ).find("h2.entry-title").height();
+    $(this).find("aside").css("top", height+26);
+  });
+});
