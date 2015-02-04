60071d50ae37fda2fb490e510cd4c7fd8f5ba6f0
Juuro <mail@sebastian-engel.de>
2015-02-05 00:23:41 +0100

    Update Publify 8.1.1. Heroku.

---
+++ public/assets/application-d0114acdd9cd04e8c99a9bd39c023c67.js
@@ -0,0 +1,14 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+
+var disqus_shortname = 'illuminationderwirklichkeitpublify'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.type = 'text/javascript';
+  s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
