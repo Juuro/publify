568321a28fe69611694d35cfe5cbb61f7ddfece1
Juuro <mail@sebastian-engel.de>
2015-07-27 00:11:20 +0200

    Adds bigfoot plugin.

--- app/assets/javascripts/publify.js
+++ app/assets/javascripts/publify.js
@@ -2,12 +2,15 @@
 //= require jquery_ujs
 //= require cookies
 //= require rails-timeago-all
+//= require bigfoot
 //= require_self
 // Front javascript manifest
 
 $(document).ready(function() {
   $(".h-entry").each(function( index ) {
     var height = $( this ).find("h2.entry-title").height();
-    $(this).find("aside").css("top", height+26);
+    $(this).find("aside").css("top", height+24);
   });
+
+  $.bigfoot();
 });
